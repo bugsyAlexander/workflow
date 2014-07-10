7a39ce9ccf084022b90011644a00d1c7f6d3975e
Mynor Alexander <sinrart@yahoo.com>
2014-07-10 15:43:38 -0700

    added compass process and modified scss files to correct pixgrid errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
 var jsSources = ['components/script/*.js'];
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function (){
 	gulp.src(jsSources)
@@ -11,3 +13,15 @@ gulp.task('js', function (){
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'));
 });
+
+gulp.task('compass', function (){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}))
+		.on('error', gutil.log)
+		.pipe(gulp.dest('builds/development/css'));
+});
+
