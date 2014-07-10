5da4fc3e74c4a54262fcc6512f4410cd7368cca0
Mynor Alexander <sinrart@yahoo.com>
2014-07-10 14:15:48 -0700

    added gulp-concat and renamed scripts.js file in components folder to script.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,11 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	concat = require('gulp-concat');
+
+var jsSources = ['components/script/*.js'];
+
+gulp.task('js', function (){
+	gulp.src(jsSources)
+		.pipe(concat('script.js'))
+		.pipe(gulp.dest('builds/development/js'));
+});
