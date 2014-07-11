985fc120fd389e8bb246d324eec3f9eb01458c2a
Mynor Alexander <sinrart@yahoo.com>
2014-07-11 16:11:46 -0700

    added Environment Variables

--- gulpfile.js
+++ gulpfile.js
@@ -4,25 +4,41 @@ var gulp = require('gulp'),
 	compass = require('gulp-compass'),
 	concat = require('gulp-concat');
 
-var jsSources = ['components/script/*.js'];
-var sassSources = ['components/sass/style.scss'];
+var env, 
+	jsSources, 
+	sassSources,
+	outputDir,
+	sassStyle;
+
+env = process.env.NODE_ENV || 'development';
+
+if (env === 'development') {
+	outputDir = 'builds/development/';
+	sassStyle = 'expanded';
+} else {
+	outputDir = 'builds/production/';
+	sassStyle = 'compressed';
+}
+
+jsSources = ['components/script/*.js'];
+sassSources = ['components/sass/style.scss'];
 
 gulp.task('js', function (){
 	gulp.src(jsSources)
 		.pipe(concat('script.js'))
 		.pipe(browserify())
-		.pipe(gulp.dest('builds/development/js'));
+		.pipe(gulp.dest( outputDir + 'js'));
 });
 
 gulp.task('compass', function (){
 	gulp.src(sassSources)
 		.pipe(compass({
 			sass: 'components/sass',
-			image: 'builds/development/images',
-			style: 'expanded'
+			image: outputDir + 'images',
+			style: sassStyle
 		}))
 		.on('error', gutil.log)
-		.pipe(gulp.dest('builds/development/css'));
+		.pipe(gulp.dest( outputDir + 'css'));
 });
 
 gulp.task('watch', function (){
